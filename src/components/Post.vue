d1220f736bf7a970f87a8acbb4e284eec98b1c46
knnonyuuki <a931919555@qq.com>
2017-08-12 22:57:42 +0800

    xg

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,35 +1,58 @@
 <template>
-  <div class="Post">
-    <div class="upper">
-      <post-body :postId="postId"></post-body>
+  <div v-if="isEmpty">
+    <div class="loading">
+      加载中...
     </div>
-    <div class="bottom">
-      <comment-box></comment-box>
+  </div>
+  <div v-else>
+    <div class="post">
+      <div class="upper">
+        <post-body :postId="postId"></post-body>
+      </div>
+      <div class="bottom">
+        <comment-box></comment-box>
+      </div>
     </div>
   </div>
 </template>
+
 <script>
   import PostBody from './PostBody'
-  import CommentBox from './Commentbox'
+  import CommentBox from './CommentBox'
+
   export default {
     name: 'post',
     components: { PostBody, CommentBox },
     computed: {
       postId: function () {
         return this.$route.params.id
+      },
+      isEmpty: function () {
+        return this.$store.state.comment.all.length === 0
       }
+    },
+    created () {
+      this.$store.dispatch('getAllPosts')
     }
   }
 </script>
+
 <style scoped>
-  .upper {
-    background-color: #00bcd4;
-    min-height: 200px;
-    display: flex;
-  }
-  .bottom {
-    display: flex;
-    background-color: #fafafa;
-    min-height: 200px;
-  }
+.upper {
+  background-color: #00bcd4;
+  min-height: 200px;
+  display: flex;
+}
+.bottom {
+  display: flex;
+  background-color: #fafafa;
+  min-height: 200px;
+}
+.loading {
+  min-height: 100vh;
+  background-color: #00bcd4;
+  color: white;
+  text-align: center;
+  padding-top: 30px;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Post from '@/components/Post'
+
 Vue.use(Router)
 
 export default new Router({
